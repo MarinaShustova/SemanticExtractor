178f83134254a59cd74df5fd541cd830a40fe2e1
Marina Shustova <Marina.Shustova@spirent.com>
2019-12-15 20:08:55 +0700

    changed file path

--- sem-proj/info.py
+++ sem-proj/info.py
@@ -7,10 +7,10 @@ def main():
     data2 = {'court of law': {'agents': ['complainant', 'Judge'], 'conflict': 'need for extra investigation', 'previous decisions': [],
                               'possible agent1 arguments': ['arg1'], 'possible agent2 arguments': ['arg2']},
              'selling a car': {'agents': ['customer', 'seller'], 'conflict': 'the absence of payment', 'previous decisions': ['customer is forsed to pay']} }
-    with open('../dialog/case.pkl', 'wb') as f1:
+    with open('case.pkl', 'wb') as f1:
         pickle.dump(data1, f1)
     f1.close()
-    with open('../dialog/Models.pkl', 'wb') as f2:
+    with open('Models.pkl', 'wb') as f2:
         pickle.dump(data2, f2)
     f2.close()
 
